d91ab31dccd11f848245a0e382749009c83c9558
ElenaRzn <aaa_rrr@mail.ru>
2021-09-03 17:23:09 +0300

    homework

---
+++ kafka_client/src/main/scala/BestsellersWithCategories.scala
@@ -0,0 +1,9 @@
+case class BestsellersWithCategories(
+                                      Name: String,
+                                      Author: String,
+                                      `User Rating`: String,
+                                      Reviews: String,
+                                      Price: Double,
+                                      Year: String,
+                                      Genre:String
+                                    )
--- kafka_client/src/main/scala/Producer.scala
+++ kafka_client/src/main/scala/Producer.scala
@@ -1,7 +1,11 @@
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
+import org.json4s.jackson.Serialization
+import org.json4s.{DefaultFormats, Formats}
 
+import java.io.{File, FileReader}
+import java.nio.file.Path
 import java.util.Properties
 
 object Producer extends App {
@@ -11,14 +15,36 @@ object Producer extends App {
 
   val producer = new KafkaProducer(props, new StringSerializer, new StringSerializer)
 
-  val messages = List(
-    "message1",
-    "message2",
-    "message3",
-  )
+  import org.apache.commons.csv.CSVFormat
+  implicit val formats: Formats = DefaultFormats
+
+//  val in = new FileReader("C:\\text\\otus\\kafka_scala_example\\kafka_client\\src\\main\\resources\\bestsellers_with_categories.csv")
+  val file = new File(getClass.getResource("bestsellers_with_categories.csv").getPath)
+  val in = new FileReader(file)
+  import scala.collection.JavaConverters._
+  val records = CSVFormat
+    .RFC4180
+    .withHeader("Name", "Author", "User Rating", "Reviews", "Price", "Year", "Genre")
+    .withSkipHeaderRecord()
+    .parse(in)
+    .iterator()
+    .asScala
+    .toList
+
+  in.close()
+
+  val messages = records.map(record => Serialization.write(BestsellersWithCategories(
+    record.get("Name"),
+    record.get("Author"),
+    record.get("User Rating"),
+    record.get("Reviews"),
+    record.get("Price").toDouble,
+    record.get("Year"),
+    record.get("Genre")
+  )))
 
   messages.foreach { m =>
-    producer.send(new ProducerRecord("mytopic", m, m))
+    producer.send(new ProducerRecord("books", m))
   }
 
   producer.close()
